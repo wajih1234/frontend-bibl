9c254bd0c8183489af9392e4919d7eaec66bbfce
wajih1234 <74155158+wajih1234@users.noreply.github.com>
2024-06-18 15:28:05 +0200

    Add files via upload

---
+++ pfa/Nouveau dossier/code.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const loginform = document.querySelector(".login-form");
+    const motdepasse = document.querySelector(".motdepasse");
+    const email = document.querySelector(".email");
+    const errorMsg = document.querySelector("#error");
+
+    const user = "kllno4@gmail.com";
+    const pass = "12345";
+    loginform.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (motdepasse.value === pass && email.value === user) {
+            window.location.href = "2m.html"; // Redirect to desired page
+        } else {
+            errorMsg.textContent = "Email or password is incorrect."; // Display error message
+        }
+    });
+});
